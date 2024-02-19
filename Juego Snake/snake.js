97137385f4f43034806534b82d753a99fb7eadb6
Manunyville <125371151+Manunyville@users.noreply.github.com>
2024-02-19 13:18:09 -0300

    css basico mas js

---
+++ Juego Snake/snake.js
@@ -0,0 +1,8 @@
+//Elementos HTML
+const board = document.getElementById("board");
+const scoreBoard = document.getElementById("scoreBoard");
+const startButton = document.getElementById("start");
+const gameOverSign = document.getElementById("gameOver");
+
+// Ajustes del juego
+
